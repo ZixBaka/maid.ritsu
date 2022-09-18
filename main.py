adcb172093f49be5ef79bcfcabd3c8456cf445d2
UwUssimo Robinson <uwussimo@icloud.com>
2022-09-18 13:59:39 +0500

    Update main.py

--- main.py
+++ main.py
@@ -7,10 +7,23 @@ from handlers.user_commands import register_user_commands
 from handlers.user_search import register_search
 from handlers.admin import register_admin
 
+WEBHOOK_HOST = 'https://ritsu.uwussi.moe'
+WEBHOOK_PATH = '/'
+WEBHOOK_URL = f"{WEBHOOK_HOST}{WEBHOOK_PATH}"
 
-async def on_startup(_):
-    print('Bot went online!')
+# webserver settings
+WEBAPP_HOST = '127.0.0.1'
+WEBAPP_PORT = 7771
+
+
+async def on_startup(bot):
+    await bot.set_webhook(WEBHOOK_URL)
     await db_start()
+
+
+async def on_shutdown(bot):
+    await bot.delete_webhook()
+
 
 register_user(dp)
 register_user_commands(dp)
@@ -20,4 +33,12 @@ register_search(dp)
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
-    executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
+    executor.start_webhook(
+        dp,
+        webhook_path=WEBHOOK_PATH,
+        skip_updates=True,
+        on_startup=on_startup,
+        on_shutdown=on_shutdown,
+        host=WEBAPP_HOST,
+        port=WEBAPP_PORT
+    )
