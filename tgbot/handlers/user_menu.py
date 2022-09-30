5df2d9d402d2f74b47a1a47c9e551dfe6dabcfe9
PC home bobosh <bobomurodm2004@gmail.com>
2022-09-30 23:04:20 +0500

    little fixes

--- tgbot/handlers/user_menu.py
+++ tgbot/handlers/user_menu.py
@@ -1,5 +1,5 @@
 from aiogram import Dispatcher
-from aiogram.types import Message, CallbackQuery
+from aiogram.types import CallbackQuery
 from aiogram.dispatcher import FSMContext
 
 from tgbot.keyboards.inline import settings_keyboard, feedback_keyboard, about_us_keyboard, main_menu_keyboard
@@ -31,7 +31,7 @@ async def about_us(call: CallbackQuery):
     await call.message.answer(f"𝐖𝐈𝐔𝐓 𝐏𝐚𝐫𝐤𝐢𝐧𝐠 𝐛𝐨𝐭\n"
                               f"This bot exists thankfully for those who contributed\n"
                               f"this project, and they are:\n\n"
-                              f"👨‍💻<a href='https://github.com/Azizbek-B'>Azizbek</a> (Main Creator, Maintainer)\n"
+                              f"👨‍💻<a href='https://github.com/Azizbek-B'>Azizbek</a> (Co-Creator, Maintainer)\n"
                               f"🕵️‍♂<a href='https://t.me/muminovbob'>Bobomurod</a> (Co-Creator, Maintainer)\n"
                               f"👩‍🚀<a href='https://github.com/uwussimo'>UwUssimo</a> (Core Contributor)\n\n"
                               f"Copyright © 2022 <a href='https://github.com/mad-maids'>Mad Maids</a>",
